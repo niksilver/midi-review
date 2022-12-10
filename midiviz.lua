3caab7130ccbd4704fe0d91ac622272cca35317d
Nik Silver <nik@niksilver.com>
2022-12-10 18:16:25 +0000

    Show note number on screen.

--- midiviz.lua
+++ midiviz.lua
@@ -2,8 +2,16 @@
 --
 -- Visualisation of MIDI keys
 
+-- Our MIDI device
+
+m = midi.connect()
+
+-- Last MIDI note number
+
+note_num = nil
+
 function init()
-    print("In the init function")
+    print("In the init function 2")
 end
 
 -- Display something on the screen.
@@ -12,6 +20,16 @@ function redraw()
     screen.clear()
     screen.move(0, 40)
     screen.level(15)
-    screen.text("Welcome to midiviz")
+    screen.text("Note: " .. (note_num or "-"))
     screen.update()
+end
+
+-- Capture current MIDI data
+--
+m.event = function(data)
+    local msg = midi.to_msg(data)
+    if msg.type == "note_on" then
+        note_num = msg.note
+    end
+    redraw()
 end
