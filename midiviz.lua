f756371d93d7a74cb07fe8f06c8c32a196a9b66e
Nik Silver <nik@niksilver.com>
2022-12-10 18:00:17 +0000

    Write something on the screen.

--- midiviz.lua
+++ midiviz.lua
@@ -5,3 +5,13 @@
 function init()
     print("In the init function")
 end
+
+-- Display something on the screen.
+--
+function redraw()
+    screen.clear()
+    screen.move(0, 40)
+    screen.level(15)
+    screen.text("Welcome to midiviz")
+    screen.update()
+end
