7eb06cec4516331fdeec2d7a5331631105b4dc80
Nik Silver <nik@niksilver.com>
2022-12-16 11:30:19 +0000

    Flesh out idx_ndata more.

--- idx_ndata.lua
+++ idx_ndata.lua
@@ -9,11 +9,16 @@
 
 C = {}
 
--- Create a new mapping
+-- Create a new sequence.
+-- @param fn    The function to call to get the current time.
+--     We don't use a default to force the programmer to be explicit.
 --
-function C.new()
+function C.new(fn)
     local obj = {}
     setmetatable(obj, {__index = C})
+
+    obj.timefn = fn
+    obj.ndata = {}
 
     return obj
 end
@@ -23,6 +28,29 @@ end
 function C:length()
     return #self.ndata
 end
+
+-- Append some note data, together with the current clock time.
+-- @param data    A map from MIDI note values (0-127) to velocity.
+--
+function C:append(data)
+    local last_index = self:length()
+    self.ndata[last_index+1] = {
+        time = self:timefn(),
+        note_vel = data,
+    }
+end
+
+-- Get the note data at a given index.
+-- @param i    Index of the data.
+-- @return    A table with keys `time` and `note_vel`.
+function C:get(i)
+    return self.ndata[i]
+end
+
+-- The function used to get the time when we append data.
+-- Nil by default
+--
+C.timefn = nil
 
 -- The map from index to time/note_vel
 --
--- test_idx_ndata.lua
+++ test_idx_ndata.lua
@@ -3,10 +3,30 @@ lu = require('luaunit')
 idx_ndata = require('idx_ndata')
 
 function test_count_normally()
-    local nd = idx_ndata.new()
+    local nd = idx_ndata.new(os.clock)
 
     -- Length of a new map should be zero
     lu.assertEquals(nd:length(), 0)
+
+    -- Appending data should increment the length
+    nd:append({[1] = 1})
+    lu.assertEquals(nd:length(), 1)
+    nd:append({[2] = 2})
+    lu.assertEquals(nd:length(), 2)
+end
+
+function test_retrieve_data_and_time()
+    local nd = idx_ndata.new(os.clock)
+
+    local time = nd.timefn()
+    nd:append({[11] = 111, [22] = 112})
+    lu.assertEquals(nd:get(1).note_vel, {[11] = 111, [22] = 112})
+    lu.assertAlmostEquals(nd:get(1).time, time, 0.1)
+
+    time = nd.timefn()
+    nd:append({[33] = 113, [44] = 114})
+    lu.assertEquals(nd:get(2).note_vel, {[33] = 113, [44] = 114})
+    lu.assertAlmostEquals(nd:get(2).time, time, 0.1)
 end
 
 os.exit(lu.LuaUnit.run())
