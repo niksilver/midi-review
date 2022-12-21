01234702c66c689c99a7d5b3f040e5dec3cfc660
Nik Silver <nik@niksilver.com>
2022-12-21 09:30:52 +0000

    Add rolling_window:delta().

--- lib/rolling_window.lua
+++ lib/rolling_window.lua
@@ -26,6 +26,23 @@ function C:size()
     return self.sizes[self.current_index]
 end
 
+-- Change the current index by some delta. Any attempt to go beyond
+-- the bounds of the size array will just keep us within the bounds.
+-- @param d    Amount to adjust the current index.
+--
+function C:delta(d)
+    -- We can't use util.clamp or math.clamp due to version and library
+    -- discrepancies between a norns any our local development box.
+
+    self.current_index = self.current_index + d
+
+    if self.current_index < 1 then
+        self.current_index = 1
+    elseif self.current_index > #self.sizes then
+        self.current_index = #self.sizes
+    end
+end
+
 -- Index of the current windows size.
 --
 C.current_index = nil
--- lib/test_rolling_window.lua
+++ lib/test_rolling_window.lua
@@ -36,3 +36,29 @@ function test_size()
 
     lu.assertEquals(window:size(), 11)
 end
+
+function test_delta()
+    local window = rw.new({ 11, 22, 33 }, 1)
+
+    lu.assertEquals(window:size(), 11)
+
+    -- As we add one to the index we should go through the sizes,
+    -- but not go beyond the end of the array.
+
+    window:delta(1)
+    lu.assertEquals(window:size(), 22)
+    window:delta(1)
+    lu.assertEquals(window:size(), 33)
+    window:delta(1)
+    lu.assertEquals(window:size(), 33)
+
+    -- And as we add -1 to the index we should go back through the sizes,
+    -- but not go beyond the start of the array.
+
+    window:delta(-1)
+    lu.assertEquals(window:size(), 22)
+    window:delta(-1)
+    lu.assertEquals(window:size(), 11)
+    window:delta(-1)
+    lu.assertEquals(window:size(), 11)
+end
